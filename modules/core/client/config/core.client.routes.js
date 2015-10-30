66b964535aeac17decd463924addc2ee6e6c2a90
Ricky Rodriguez <ricardodrodrigue@ufl.edu>
2015-10-30 07:57:46 -0400

    commented out areas and deleted for testing purposes
    display issue still not fixed

--- modules/core/client/config/core.client.routes.js
+++ modules/core/client/config/core.client.routes.js
@@ -18,7 +18,7 @@ angular.module('core').config(['$stateProvider', '$urlRouterProvider',
             "content": {
                 templateUrl: "modules/patients/views/overview.client.view.html"
             }
-        },
+        }/*,
         controller: function($state, Authentication){
         	if(Authentication.users){
         		// dont do anything for now
@@ -26,7 +26,7 @@ angular.module('core').config(['$stateProvider', '$urlRouterProvider',
         		else{
         			$state.go('home.public');
         		}
-        	}
+        	}*/
       })
       .state('home.signedin', {
       	url:'',
@@ -49,7 +49,7 @@ angular.module('core').config(['$stateProvider', '$urlRouterProvider',
         templateUrl: 'modules/core/views/404.client.view.html'
       })
 	  .state('overview', {
-            url: "/overview",
+            url: '/overview',
             views: {
                 "sidebar": {
                     templateUrl: "/modules/core/views/sidebar.client.view.html"
@@ -60,7 +60,7 @@ angular.module('core').config(['$stateProvider', '$urlRouterProvider',
             }
       })
 	  .state('progressForms', {
-            url: "/progressForms",
+            url: '/progressForms',
             views: {
                 "sidebar": {
                     templateUrl: "/modules/core/views/sidebar.client.view.html"
@@ -108,6 +108,70 @@ angular.module('core').config(['$stateProvider', '$urlRouterProvider',
                 }
             }
       })
+      //begin adding new states from users routes folder
+      .state('settings', {
+          abstract: true,
+          url: '/settings',
+          templateUrl: 'modules/users/views/settings/settings.client.view.html',
+        data: {
+          roles: ['user', 'admin']
+        }
+      })
+      .state('settings.profile', {
+        url: '/profile',
+        templateUrl: 'modules/users/views/settings/edit-profile.client.view.html'
+      })
+      .state('settings.password', {
+        url: '/password',
+        templateUrl: 'modules/users/views/settings/change-password.client.view.html'
+      })
+      .state('settings.accounts', {
+        url: '/accounts',
+        templateUrl: 'modules/users/views/settings/manage-social-accounts.client.view.html'
+      })
+      .state('settings.picture', {
+        url: '/picture',
+        templateUrl: 'modules/users/views/settings/change-profile-picture.client.view.html'
+      })
+      .state('authentication', {
+        abstract: true,
+        url: '/authentication',
+        templateUrl: 'modules/users/views/authentication/authentication.client.view.html'
+      })
+      .state('authentication.signup', {
+        url: '/signup',
+        templateUrl: 'modules/users/views/authentication/signup.client.view.html'
+      })
+      .state('authentication.signin', {
+        url: '/signin?err',
+        templateUrl: 'modules/users/views/authentication/signin.client.view.html'
+      })
+      .state('password', {
+        abstract: true,
+        url: '/password',
+        template: '<ui-view/>'
+      })
+      .state('password.forgot', {
+        url: '/forgot',
+        templateUrl: 'modules/users/views/password/forgot-password.client.view.html'
+      })
+      .state('password.reset', {
+        abstract: true,
+        url: '/reset',
+        template: '<ui-view/>'
+      })
+      .state('password.reset.invalid', {
+        url: '/invalid',
+        templateUrl: 'modules/users/views/password/reset-password-invalid.client.view.html'
+      })
+      .state('password.reset.success', {
+        url: '/success',
+        templateUrl: 'modules/users/views/password/reset-password-success.client.view.html'
+      })
+      .state('password.reset.form', {
+        url: '/:token',
+        templateUrl: 'modules/users/views/password/reset-password.client.view.html'
+      });
 
   }
 

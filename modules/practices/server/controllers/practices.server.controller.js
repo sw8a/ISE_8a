9562e23cdb6b3bfc189657d2748fc3f406421ad9
willbc <wccase@live.com>
2015-12-06 14:43:51 -0500

    Fixed enrollment form saving, populate pet owners on vet homepage

--- modules/practices/server/controllers/practices.server.controller.js
+++ modules/practices/server/controllers/practices.server.controller.js
@@ -24,29 +24,31 @@ exports.saveNewPractice = function (req, res) {
 
 exports.updatePractice = function (req, res) {
     var practice = req.body;
+    var changedData = practice.changedData;
+    var practiceId = practice._id;
+    delete practice.changedData;
+    delete practice._id;
 
-    if(practice.newPatient) {
-
-        Practice.findByIdAndUpdate(
-            practice._id,
-            {
-                $push: { 'patients': practice.newPatient } 
-            },
-            {
-                safe: true,
-                new: true
-            },
-            function(err) {
-                if (err) {
-                    return res.status(400).send({
-                        message: errorHandler.getErrorMessage(err)
-                    });
-                } else {
-                    res.json(practice);
-                }
+    Practice.findByIdAndUpdate(
+        practiceId,
+        {
+            $push: { 'changedData': changedData },
+            $set: practice
+        },
+        {
+            safe: true,
+            new: true
+        },
+        function(err) {
+            if (err) {
+                return res.status(400).send({
+                    message: errorHandler.getErrorMessage(err)
+                });
+            } else {
+                res.json(practice);
             }
-        );
-    }
+        }
+    );
 };
 
 exports.getPractice = function (req, res) {
@@ -71,7 +73,23 @@ exports.practiceById = function (req, res, next, id) {
                 message: 'No practice found'
             });
         }
-        req.practice = foundPractice;
-        next();
+
+        var options = {
+            path: 'patients.petOwner',
+            model: 'PetOwner'
+        };
+
+        Practice.populate(foundPractice, options, function (err, populatedPractice) {
+            if (err) {
+                return next(err);
+            } 
+            else if (!populatedPractice) {
+                return res.status(404).send({
+                    message: 'Practice population error'
+                });
+            }
+            req.practice = foundPractice;
+            next();
+        });
     });
 };
